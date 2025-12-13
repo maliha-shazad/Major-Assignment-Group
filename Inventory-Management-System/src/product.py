edb0a3d46be0aeae4ddd94e01749ab495a9a7349
momal-shah <syedamomal55@gmail.com>
2025-12-13 15:35:16 +0500

    Add display method to Product class
    implemented Product class and display method

--- Inventory-Management-System/src/product.py
+++ Inventory-Management-System/src/product.py
@@ -1,7 +1,13 @@
+
+
 class Product:
     def __init__(self, product_id, name, price, quantity):
         self.product_id = product_id
         self.name = name
         self.price = price
         self.quantity = quantity
+
+    def display(self):
+        return f"{self.product_id} | {self.name} | {self.price} | {self.quantity}"
+
 
