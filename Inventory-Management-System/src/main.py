9f60b8ed1c825801437a1685b109eac511756b6f
Bakhtawar853 <bakhtawarm209@gmail.com>
2025-12-13 15:44:43 +0500

    Update main.py
    updated menu with search and delete options

--- Inventory-Management-System/src/main.py
+++ Inventory-Management-System/src/main.py
@@ -1,5 +1,3 @@
-
-
 from inventory import Inventory
 from product import Product
 
@@ -9,7 +7,9 @@ def show_menu():
     print("\n===== Inventory Management System =====")
     print("1. Add Product")
     print("2. View Products")
-    print("3. Exit")
+    print("3. Search Product")
+    print("4. Delete Product")
+    print("5. Exit")
 
 while True:
     show_menu()
@@ -29,6 +29,22 @@ while True:
         inventory.view_products()
 
     elif choice == "3":
+        pid = input("Enter Product ID to search: ")
+        product = inventory.search_product(pid)
+        if product:
+            print("Product Found:")
+            print(product.display())
+        else:
+            print("Product not found.")
+
+    elif choice == "4":
+        pid = input("Enter Product ID to delete: ")
+        if inventory.delete_product(pid):
+            print("Product deleted successfully.")
+        else:
+            print("Product not found.")
+
+    elif choice == "5":
         print("Exiting system...")
         break
 
