e49af328e65c1d5fefeb9620f0256c25edfc4097
Bakhtawar853 <bakhtawarm209@gmail.com>
2025-12-13 15:36:29 +0500

    Update inventory.py
    added inventory add and view functionality

--- Inventory-Management-System/src/inventory.py
+++ Inventory-Management-System/src/inventory.py
@@ -7,3 +7,13 @@ class Inventory:
     def add_product(self, product):
         self.products.append(product)
 
+    def view_products(self):
+        if not self.products:
+            print("No products available.")
+            return
+
+        print("\nID | Name | Price | Quantity")
+        for product in self.products:
+            print(product.display())
+
+
