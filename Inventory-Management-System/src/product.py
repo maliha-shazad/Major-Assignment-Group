47efba4b9fa7479d679f9782e2cbf9cfd00af5bd
maliha-shazad <manaa9055@gmail.com>
2025-12-13 15:01:29 +0500

    Update product.py
    Added Product class with basic attributes

--- Inventory-Management-System/src/product.py
+++ Inventory-Management-System/src/product.py
@@ -1,1 +1,7 @@
+class Product:
+    def __init__(self, product_id, name, price, quantity):
+        self.product_id = product_id
+        self.name = name
+        self.price = price
+        self.quantity = quantity
 
