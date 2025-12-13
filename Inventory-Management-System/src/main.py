96d333a01c43f3ecc6722a60d3535a8dab0e95b2
maliha-shazad <manaa9055@gmail.com>
2025-12-13 15:32:27 +0500

    Update main.py
    implemented menu and user input flow

--- Inventory-Management-System/src/main.py
+++ Inventory-Management-System/src/main.py
@@ -1,7 +1,36 @@
+
+
 from inventory import Inventory
+from product import Product
 
 inventory = Inventory()
 
-print("Inventory Management System")
-print("System started successfully")
+def show_menu():
+    print("\n===== Inventory Management System =====")
+    print("1. Add Product")
+    print("2. View Products")
+    print("3. Exit")
 
+while True:
+    show_menu()
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        pid = input("Enter Product ID: ")
+        name = input("Enter Product Name: ")
+        price = float(input("Enter Price: "))
+        qty = int(input("Enter Quantity: "))
+
+        product = Product(pid, name, price, qty)
+        inventory.add_product(product)
+        print("Product added successfully!")
+
+    elif choice == "2":
+        inventory.view_products()
+
+    elif choice == "3":
+        print("Exiting system...")
+        break
+
+    else:
+        print("Invalid choice. Try again.")
